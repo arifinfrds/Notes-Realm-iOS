468eb31ec256f6ee461b757eb8fb06bd2838d607
Arifin <arifin.firdaus1996@gmail.com>
2018-02-11 17:12:50 +0700

    done ui

---
+++ Notes-Realm-iOS/helper/RandomStringGenerator.swift
@@ -0,0 +1,27 @@
+//
+//  RandomStringGenerator.swift
+//  Notes-Realm-iOS
+//
+//  Created by Arifin Firdaus on 2/11/18.
+//  Copyright © 2018 Arifin Firdaus. All rights reserved.
+//
+
+import Foundation
+
+class RandomStringGenerator {
+    
+    static func randomString(length: Int) -> String {
+        let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        let len = UInt32(letters.length)
+        
+        var randomString = ""
+        
+        for _ in 0 ..< length {
+            let rand = arc4random_uniform(len)
+            var nextChar = letters.character(at: Int(rand))
+            randomString += NSString(characters: &nextChar, length: 1) as String
+        }
+        return randomString
+    }
+    
+}
